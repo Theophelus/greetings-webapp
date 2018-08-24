1e34cf23310e7e16a3bd0b570ff6e83f81d536e9
Theophelus <aneletom10@gmail.com>
2018-08-24 18:07:34 +0200

    updating my factory function and add queries

--- greet.js
+++ greet.js
@@ -1,73 +1,80 @@
-module.exports = function (initialValue) {
+module.exports = function (pool) {
   var nameMap = {};
   let greetedNames = [];
-  var enteredName = '';
+  var checkUsersSQL = '';
   //Everytime a namae is detacted the count
-  if (initialValue) {
-    nameMap = initialValue;
-  }
+  // if (initialValue) {
+  //   nameMap = initialValue;
+  // }
   ///////////////////CHECK IF USER EXISTS IF NOT ADD////////////////////////////
   //when the greet button is pressed check if this user was already greeted before
   //by looking if the userName exists in namesGreeted if not increment this counter and update the screen
-  var setEnteredName = function (selectedLanguage, name) {
-
-    //define an object to store all the names
-    if (name === null || name == '') {
-      return 'Please enter a NAME in the text field..!';
-    } else if (selectedLanguage) {
-      language = selectedLanguage;
-    } else {
-      return 'Please select one of the languages in one of the radio buttons..!';
-    }
-    if (name !== '') {
-      enteredName = name;
-      if (nameMap[enteredName] === undefined) {
-        //add an entry for the user that was greeted in the Object Map
-        nameMap[enteredName] = 0;
-        // greetedNames.filter(function(element){});
-        greetedNames.push({
-          names: name
-        });
+  var setEnteredName = async function (language, enteredName) {
+    //define an object to store all the enteredName
+    // if (enteredName === null || enteredName == '') {
+    //   return 'Please enter a NAME in the text field..!';
+    // } else if (selectedLanguage) {
+    //   language = selectedLanguage;
+    // } else {
+    //   return 'Please select one of the languages in one of the radio buttons..!';
+    // }
+    // if (enteredName && enteredName !== '') {
+    //   if (checkUsersSQL.rawCount == 0) {
+    //     //if user then insert it to the DB using the insert query
+    //     await pool.query('insert into users() values($1, $2)', [enteredName, 1]);
+    //     //add an entry for the user that was greeted in the Object Map
+    //     // nameMap[enteredName] = 0;
+    //     // greetedNames.filter(function(element){});
+    //     // greetedNames.push({
+    //     //   enteredName: enteredName
+    //     // });
+    //   }
+    if (enteredName && enteredName !== '') {
+      checkUsersSQL = await pool.query('select * from users where user_name=$1', [enteredName]);
+      if (checkUsersSQL.rowCount === 0) {
+        //if user then insert it to the DB using the insert query
+        await pool.query('insert into users(user_name, user_count) values ($1, $2)', [enteredName, 1]);
       }
+      await pool.query('update users set user_count = user_count + 1  where user_name = $1', [enteredName]);
+
       ////////////////////CHECK TYPE OF LANGUAGE SELECTED AN GREET USER/////////////////
       if (language === 'English') {
         return 'Hello,' + ' ' + enteredName;
       }
-
       if (language === 'IsiXhosa') {
-        return 'Molo,' + ' ' + name;
+        return 'Molo,' + ' ' + enteredName;
       }
 
       if (language === 'Afrikaans') {
 
-        return 'Goeie Dag,' + ' ' + name;
+        return 'Goeie Dag,' + ' ' + enteredName;
       } else {
         return '';
       }
     }
   };
-  let filterNames = function (user) {
-    return greetedNames.filter(element => element.names == user);
-  };
+  // let filterNames = function (user) {
+  //   return greetedNames.filter(element => element.enteredName == user);
+  // };
   ////////////////////////END TO CHECK IF THE USER EXIST////////////////////////////
-  var getEnteredNameCount = function () {
-    return Object.keys(nameMap).length;
+  var getEnteredNameCount = async function (counter) {
+    await pool.query('select count(*) from users where user_count=$1', [counter]);
   };
-  var map = function () {
-    return nameMap;
-  };
-  let getGreetedNames = function () {
-    return greetedNames;
-  };
-  var resetData = function () {
-    return nameMap == {};
-  };
+  // var map = function () {
+  //   return nameMap;
+  // };
+  // let getGreetedNames = function () {
+  //   return greetedNames;
+  // };
+  // var resetData = function () {
+  //   return nameMap == {};
+  // };
   return {
-    getGreetedNames,
+    // getGreetedNames,
     setEnteredName,
     getEnteredNameCount,
     // filterNames,
-    resetData,
-    map
+    // resetData,
+    // map
   };
 };
--- index.js
+++ index.js
@@ -3,34 +3,27 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const Greetings = require('./greet');
 const bodyParser = require('body-parser');
+// const session = require('express-session');
 const pg = require("pg");
 const Pool = pg.Pool;
 
 //define instances
 let app = express();
-let greets = Greetings();
 
 // should we use a SSL connection
-let useSSL = false;
+// let useSSL = false;
 
 let local = process.env.LOCAL || false;
-if (process.env.DATABASE_URL && !local){
+if (process.env.DATABASE_URL && !local) {
     useSSL = true;
 }
-
 //define a connection string to be able to connect to the database.
-const connectionString = process.env.DATABASE_URL || 'coder:pg123@postgresql://localhost:5432/greetings';
+const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/greetings';
 const pool = new Pool({
-    connectionString,
-    ssl : useSSL
+    connectionString
+    // ssl: useSSL
 });
-app.use(session({
-    secret: 'keyboard cat5 run all 0v3r',
-    resave: false,
-    saveUninitialized: true
-}));
-
-
+let greets = Greetings(pool);
 
 //configure express handlebars
 app.engine('handlebars', exphbs({
@@ -52,32 +45,36 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 //define a POST route handler to handle sumbitted info in the form
-app.post('/greetings', (req, res) => {
-    let enteredName = req.body.nameInput;
-    let selectLanguage = req.body.whichLanguage;
-    //define an object with key value pair to store inputs and render that data to home
-    res.render('home', {
-        display: greets.setEnteredName(selectLanguage, enteredName),
-        count: greets.getEnteredNameCount()
-    });
-});
-//define a GET route handler handle greeted users
-app.get('/greeted/', (req, res) => {
-    let enterName = greets.getGreetedNames();
-    if (enterName !== undefined) {
-        res.render('greeted', {
-            names: enterName
+app.post('/greetings', async (req, res, next) => {
+    try {
+        // let enteredName = req.body.nameInput;
+        // let selectLanguage = req.body.whichLanguage;
+        //define an object with key value pair to store inputs and render that data to home
+        res.render('home', {
+            display: await greets.setEnteredName(req.body.whichLanguage, req.body.nameInput),
+            counter: await greets.getEnteredNameCount()
         });
+    } catch (error) {
+        next(error.stack);
     }
 });
-//define a GET route handler to check how many times a user have been greeted
-app.get('/counter/:users', (req, res) => {
-    let users = req.params.users;
-    res.render('counter',
-        greets.filterNames(users)
-    );
-    console.log(greets.filterNames());
-});
+//define a GET route handler handle greeted users
+// app.get('/greeted/', (req, res) => {
+//     let enterName = greets.getGreetedNames();
+//     if (enterName !== undefined) {
+//         res.render('greeted', {
+//             names: enterName
+//         });
+//     }
+// });
+// //define a GET route handler to check how many times a user have been greeted
+// app.get('/counter/:users', (req, res) => {
+//     let users = req.params.users;
+//     res.render('counter',
+//         greets.filterNames(users)
+//     );
+//     console.log(greets.filterNames());
+// });
 let PORT = process.env.PORT || 3009;
 app.listen(PORT, function () {
     console.log('App starting on port', PORT);
